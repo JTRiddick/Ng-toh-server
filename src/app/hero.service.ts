c3bff11e6e8f16fd1d84e66242e3784d80399dec
Taylor <jordan.riddick@utsouthwestern.edu>
2018-05-07 17:27:13 -0500

    services: generate component messages

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service'
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+
+getHeroes(): Observable<Hero[]>{
+   // TODO: send the message _after_ fetching the heroes
+
+   return of(HEROES);
+ }
+}
