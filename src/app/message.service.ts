4f852ff73bc0a4b2bbf4477f35fac4d3c2720c9f
Taylor <jordan.riddick@utsouthwestern.edu>
2018-05-08 10:42:54 -0500

    section 6 finished

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   messages: string[] = [];
+
+  constructor(public messageService: MessageService) {}
 
   add(message: string) {
     this.messages.push(message);
